471d4f118a578b0ae4985e2e68cd5c10e51017ea
Derek Battams <derek@battams.ca>
2016-04-24 10:36:14 -0400

    Adjust the width of the Transaction column output dynamically
    Adjust the width of the tx column based on the longest name found.  The
    entire row width is limited to 78 chars to avoid wrapping on most
    terminals.  Any tx name longer than the max available width will be
    truncated with an ellipsis to maintain the formatting.

--- src/main/java/org/roy/loadx/transaction/TransactionPrinter.java
+++ src/main/java/org/roy/loadx/transaction/TransactionPrinter.java
@@ -1,5 +1,8 @@
 package org.roy.loadx.transaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionPrinter {
 	private final TransactionAggregator transactionAggregator;
 
@@ -19,19 +22,38 @@ public class TransactionPrinter {
 	}
 
 	public void print() {
-		printFormatted("Transaction", "Average", "Count");
-		printFormatted("===========", "=======", "=====");
+		List<String[]> output = new ArrayList<>();
+		output.add(new String[] {"Transaction", "Average", "Count"});
+		int maxTxLength = 11; // length of 'Transaction'
+		
 		for (String name : transactionAggregator.getSortedTransactionNames()) {
 			TransactionData transactionData = transactionAggregator.getTransactionData(name);
 			long roundedAverage = Math.round(transactionData.getAverageDurationMilli());
 			String average = String.valueOf(roundedAverage);
 			String count = String.valueOf(transactionData.getTransactionCount());
-			printFormatted(name, average, count);
+			output.add(new String[] {name, average, count});
+			if(name.length() > maxTxLength)
+				maxTxLength = name.length();
 		}
-		println();
+		printFormatted(output, maxTxLength);
 	}
 
-	private void printFormatted(String name, String count, String average) {
-		format("%-15s | %10s | %10s", name, count, average);
+	private void printFormatted(final List<String[]> data, final int maxTxLength) {
+		int rowWidth = 78; // adjust to taste
+		int txWidth = Math.min(rowWidth - 26, maxTxLength); // 26 is based on space needed for other cols using existing format
+		
+		// insert the header separator based on the dynamically generated length
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < txWidth; ++i)
+			sb.append("=");
+		data.add(1, new String[] {sb.toString(), "==========", "=========="});
+
+		String fmtString = String.format("%%-%ds | %%10s | %%10s", txWidth);
+		for(String[] row : data) {
+			if(row[0].length() > txWidth)
+				row[0] = row[0].substring(0, txWidth - 3) + "...";
+			format(fmtString, row[0], row[1], row[2]);
+		}
+		println();
 	}
 }
