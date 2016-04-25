5329ba33ed8b6282cd7be75e22af7bdf39b468db
Derek Battams <derek@battams.ca>
2016-04-25 09:43:44 -0400

    Updates based on review
    Create proper classes for summary table (TransactionDataSummary) and
    table rows (TransactionDataSummaryEntry).
    
    Create proper statics for class configuration values.
    
    Use commons-lang where appropriate.
    
    Address coding style issues.

--- src/main/java/org/roy/loadx/transaction/TransactionPrinter.java
+++ src/main/java/org/roy/loadx/transaction/TransactionPrinter.java
@@ -1,59 +1,101 @@
 package org.roy.loadx.transaction;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 
 public class TransactionPrinter {
+	static private class TransactionDataSummaryEntry {
+		private String transactionName;
+		private String average;
+		private String count;
+		
+		public TransactionDataSummaryEntry(String transactionName, String average, String count) {
+			this.transactionName = transactionName;
+			this.average = average;
+			this.count = count;
+		}
+
+		public String getTransactionName() {
+			return transactionName;
+		}
+
+		public String getAverage() {
+			return average;
+		}
+
+		public String getCount() {
+			return count;
+		}
+	}
+	
+	static private class TransactionDataSummary {
+		static private final String TRANSACTION_HEADER = "Transaction";
+		static private final String AVERAGE_HEADER = "Average";
+		static private final String COUNT_HEADER = "Count";
+		static private final int MAX_ROW_WIDTH = 78;
+		static private final String ROW_FORMAT = "%%-%ds | %%10s | %%10s%n";
+		static private final int PRE_ALLOCATED_SPACE_PER_ROW = 26;
+		
+		private int maxTransactionNameLength;
+		private ArrayList<TransactionDataSummaryEntry> rows;
+		
+		public TransactionDataSummary() {
+			maxTransactionNameLength = TRANSACTION_HEADER.length();
+			rows = new ArrayList<TransactionDataSummaryEntry>();
+		}
+		
+		public void add(TransactionDataSummaryEntry entry) {
+			rows.add(entry);
+			int txNameLength = entry.getTransactionName().length();
+			if (txNameLength > maxTransactionNameLength) {
+				maxTransactionNameLength = txNameLength;
+			}
+		}
+		
+		@Override
+		public String toString() {
+			int txColumnWidth = getTransactionColumnWidth();
+			String rowFormat = computeDynamicFormatString();
+			StringBuilder sb = new StringBuilder();
+			sb.append(String.format(rowFormat, TRANSACTION_HEADER, AVERAGE_HEADER, COUNT_HEADER));
+			sb.append(String.format(rowFormat, StringUtils.repeat("=", txColumnWidth), StringUtils.repeat("=", AVERAGE_HEADER.length()), StringUtils.repeat("=", COUNT_HEADER.length())));
+			for (TransactionDataSummaryEntry entry : rows) {
+				String name = entry.getTransactionName();
+				if (name.length() > txColumnWidth) {
+					name = name.substring(0, txColumnWidth - 3) + "...";
+				}
+				sb.append(String.format(rowFormat, name, entry.getAverage(), entry.getCount()));
+			}
+			return sb.toString();
+		}
+		
+		private String computeDynamicFormatString() {
+			return String.format(ROW_FORMAT, getTransactionColumnWidth());
+		}
+		
+		private int getTransactionColumnWidth() {
+			return Math.min(MAX_ROW_WIDTH - PRE_ALLOCATED_SPACE_PER_ROW, maxTransactionNameLength);
+		}
+	}
+	
 	private final TransactionAggregator transactionAggregator;
 
 	public TransactionPrinter(TransactionAggregator transactionAggregator) {
 		this.transactionAggregator = transactionAggregator;
 	}
 
-	private static void println() {
-		System.out.println();
-		System.out.flush();
-	}
-
-	private static void format(String format, Object... args) {
-		System.out.format(format, args);
-		System.out.flush();
-		println();
-	}
-
 	public void print() {
-		List<String[]> output = new ArrayList<>();
-		output.add(new String[] {"Transaction", "Average", "Count"});
-		int maxTxLength = 11; // length of 'Transaction'
-		
+		TransactionDataSummary summary = new TransactionDataSummary();
 		for (String name : transactionAggregator.getSortedTransactionNames()) {
 			TransactionData transactionData = transactionAggregator.getTransactionData(name);
 			long roundedAverage = Math.round(transactionData.getAverageDurationMilli());
 			String average = String.valueOf(roundedAverage);
 			String count = String.valueOf(transactionData.getTransactionCount());
-			output.add(new String[] {name, average, count});
-			if(name.length() > maxTxLength)
-				maxTxLength = name.length();
+			summary.add(new TransactionDataSummaryEntry(name, average, count));
 		}
-		printFormatted(output, maxTxLength);
-	}
-
-	private void printFormatted(final List<String[]> data, final int maxTxLength) {
-		int rowWidth = 78; // adjust to taste
-		int txWidth = Math.min(rowWidth - 26, maxTxLength); // 26 is based on space needed for other cols using existing format
-		
-		// insert the header separator based on the dynamically generated length
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < txWidth; ++i)
-			sb.append("=");
-		data.add(1, new String[] {sb.toString(), "==========", "=========="});
-
-		String fmtString = String.format("%%-%ds | %%10s | %%10s", txWidth);
-		for(String[] row : data) {
-			if(row[0].length() > txWidth)
-				row[0] = row[0].substring(0, txWidth - 3) + "...";
-			format(fmtString, row[0], row[1], row[2]);
-		}
-		println();
+		System.out.println(summary);
+		System.out.flush();
+		System.out.println();
 	}
 }
