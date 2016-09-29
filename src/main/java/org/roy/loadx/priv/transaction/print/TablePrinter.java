35d8475019075b66c0f5b32ac309234e79c013f4
Patrick Roy <patrick.roy@paysafe.com>
2016-09-29 10:33:19 -0400

    Re-wrote transaction table printing

---
+++ src/main/java/org/roy/loadx/priv/transaction/print/TablePrinter.java
@@ -0,0 +1,99 @@
+package org.roy.loadx.priv.transaction.print;
+
+import com.google.common.base.Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TablePrinter {
+  private final List<String> headerRow = new ArrayList<>();
+  private final List<List<String>> dataRows = new ArrayList<>();
+  private final List<Integer> columnMaxWidths = new ArrayList<>();
+
+  public TablePrinter(String... columnHeaders) {
+    for (String columnHeader : columnHeaders) {
+      this.headerRow.add(columnHeader);
+    }
+  }
+
+  public void addRow(String... columnValues) {
+    List<String> dataRow = new ArrayList<>();
+    for (String columnValue : columnValues) {
+      dataRow.add(columnValue);
+    }
+    dataRows.add(dataRow);
+  }
+
+  private void updateColumnMaxWidthsForRow(List<String> row) {
+    int index = 0;
+    for (String value : row) {
+      if (index >= columnMaxWidths.size()) {
+        columnMaxWidths.add(value.length());
+      } else {
+        if (columnMaxWidths.get(index) < value.length()) {
+          columnMaxWidths.set(index, value.length());
+        }
+      }
+      index++;
+    }
+  }
+
+  private void setColumnMaxWidths() {
+    updateColumnMaxWidthsForRow(headerRow);
+    for (List<String> dataRow : dataRows) {
+      updateColumnMaxWidthsForRow(dataRow);
+    }
+  }
+
+  private String getRowFormat(String separator) {
+    StringBuilder rowFormat = new StringBuilder();
+    boolean first = true;
+    for (int columnMaxWidth : columnMaxWidths) {
+      if (!first) {
+        rowFormat.append(separator);        
+      }
+      rowFormat.append("%" + (first ? "-" : "") + columnMaxWidth + "s");
+      first = false;
+    }
+    return rowFormat.toString();
+  }
+
+  private static void println(String str) {
+    System.out.println(str);
+    System.out.flush();
+  }
+
+  private List<String> getLayoutRow() {
+    List<String> layoutRow = new ArrayList<>();
+    for (int columnMaxWidth : columnMaxWidths) {
+      layoutRow.add(Strings.repeat("-", columnMaxWidth));
+    }
+    return layoutRow;
+  }
+
+  private void printRow(String rowFormat, List<String> row) {
+    println(String.format(rowFormat, row.toArray()));
+  }
+
+  private void printTableFirstAndLastRow() {
+    printRow(getRowFormat("---"), getLayoutRow());
+  }
+
+  public void print() {
+    setColumnMaxWidths();
+
+    printTableFirstAndLastRow();
+    
+    String separatedRowFormat = getRowFormat(" | ");
+    printRow(separatedRowFormat, headerRow);
+    printRow(getRowFormat("-|-"), getLayoutRow());
+
+    for (List<String> dataRow : dataRows) {
+      printRow(separatedRowFormat, dataRow);
+    }
+    
+    if (dataRows.size() > 0) {
+      printTableFirstAndLastRow();
+    }
+  }
+}
--- src/main/java/org/roy/loadx/priv/transaction/print/TransactionPrinterImpl.java
+++ src/main/java/org/roy/loadx/priv/transaction/print/TransactionPrinterImpl.java
@@ -1,97 +1,11 @@
 package org.roy.loadx.priv.transaction.print;
 
-import org.apache.commons.lang.StringUtils;
 import org.roy.loadx.priv.transaction.TransactionAggregator;
 import org.roy.loadx.priv.transaction.TransactionData;
 
-import java.util.ArrayList;
-
 public class TransactionPrinterImpl implements TransactionPrinter {
 
-  /**
-   * Immutable.
-   */
-  static private class TransactionDataSummaryEntry {
-    public final String transactionName;
-    public final String average;
-    public final String pass;
-    public final String fail;
-
-    public TransactionDataSummaryEntry(String transactionName, String average, String pass,
-        String fail) {
-      this.transactionName = transactionName;
-      this.average = average;
-      this.pass = pass;
-      this.fail = fail;
-    }
-  }
-
-  static private class TransactionDataSummary {
-    static private final String TRANSACTION_HEADER = "Transaction";
-    static private final String AVERAGE_HEADER = "Average";
-    static private final String PASS_HEADER = "Pass";
-    static private final String FAIL_HEADER = "Fail";
-    static private final int MAX_ROW_WIDTH = 120;
-    static private final int CONSOLE_MAX_ROW_WIDTH = MAX_ROW_WIDTH - 1;
-    static private final String ROW_FORMAT = "%%-%ds | %%10s | %%10s | %%10s%n";
-    static private final int PRE_ALLOCATED_SPACE_PER_ROW = 26;
-    static private final String ELLIPSIS = "...";
-
-    private int longestTransactionNameLength;
-    private ArrayList<TransactionDataSummaryEntry> rows = new ArrayList<>();
-
-    public TransactionDataSummary() {
-      longestTransactionNameLength = TRANSACTION_HEADER.length();
-    }
-
-    public void add(TransactionDataSummaryEntry entry) {
-      rows.add(entry);
-      int transactionNameLength = entry.transactionName.length();
-      if (transactionNameLength > longestTransactionNameLength) {
-        longestTransactionNameLength = transactionNameLength;
-      }
-    }
-
-    @Override
-    public String toString() {
-      int transactionColumnWidth = getTransactionColumnWidth();
-      String rowFormat = computeDynamicFormatString();
-      StringBuilder sb = new StringBuilder();
-      sb.append(String.format(rowFormat, TRANSACTION_HEADER, AVERAGE_HEADER, PASS_HEADER,
-          FAIL_HEADER));
-      sb.append(String.format(rowFormat, StringUtils.repeat("=", transactionColumnWidth),
-          StringUtils.repeat("=", AVERAGE_HEADER.length()),
-          StringUtils.repeat("=", PASS_HEADER.length()),
-          StringUtils.repeat("=", FAIL_HEADER.length())));
-      for (TransactionDataSummaryEntry entry : rows) {
-        sb.append(String.format(rowFormat, getTransactionDisplayName(entry.transactionName),
-            entry.average, entry.pass, entry.fail));
-      }
-      return sb.toString();
-    }
-
-    private String computeDynamicFormatString() {
-      return String.format(ROW_FORMAT, getTransactionColumnWidth());
-    }
-
-    private int getTransactionColumnWidth() {
-      return Math.min(CONSOLE_MAX_ROW_WIDTH - PRE_ALLOCATED_SPACE_PER_ROW,
-          longestTransactionNameLength);
-    }
-
-    private String getTransactionDisplayName(String transactionName) {
-      int transactionColumnWidth = getTransactionColumnWidth();
-      return transactionName.length() > transactionColumnWidth ? transactionName.substring(0,
-          transactionColumnWidth - ELLIPSIS.length()) + ELLIPSIS : transactionName;
-    }
-  }
-
   private final TransactionAggregator transactionAggregator;
-
-  static private void println(String str) {
-    System.out.println(str);
-    System.out.flush();
-  }
 
   TransactionPrinterImpl(TransactionAggregator transactionAggregator) {
     this.transactionAggregator = transactionAggregator;
@@ -99,15 +13,15 @@ public class TransactionPrinterImpl implements TransactionPrinter {
 
   @Override
   public void print() {
-    TransactionDataSummary summary = new TransactionDataSummary();
+    TablePrinter tablePrinter = new TablePrinter("Transaction", "Average", "Pass", "Fail");
     for (String name : transactionAggregator.getSortedTransactionNames()) {
       TransactionData transactionData = transactionAggregator.getTransactionData(name);
       long roundedAverage = Math.round(transactionData.getAverageDurationMilli());
       String average = String.valueOf(roundedAverage);
       String pass = String.valueOf(transactionData.getPassCount());
       String fail = String.valueOf(transactionData.getFailCount());
-      summary.add(new TransactionDataSummaryEntry(name, average, pass, fail));
+      tablePrinter.addRow(name, average, pass, fail);
     }
-    println(summary.toString());
+    tablePrinter.print();
   }
 }
